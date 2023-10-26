9f2db1bda1c150f890c564924405c6d9a05f62bf
Pontus Berglund <pontusberglund@hotmail.com>
2023-10-26 02:07:16 +0200

    Class costumer

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Costumer
+{
+    public string Name { get; set; }
+    public string PhonneNum { get; set; }
+    public int CostumerNum { get; set; }
+
+    public Costumer(string name, string phonenum, int costumernum)
+    {
+        Name = name;
+        PhonneNum = phonenum;
+        CostumerNum = costumernum
+    }
+
+    public void ShowInformation()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age} years, Costumernumber: {CostumerNum}");
+    }
+}
