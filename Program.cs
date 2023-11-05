8622ced9e26e350d4665a3e3f22cbd69b7f482ec
Pontus Berglund <pontusberglund@hotmail.com>
2023-11-05 16:25:13 +0100

    menu without methods

--- Program.cs
+++ Program.cs
@@ -38,32 +38,95 @@ class Program
     static void Main()
     {
         
-        Customer customer = new Customer("Alice", "123-456-7890", 748693);
-        
-        Console.WriteLine("Do you want to do a Review");
-        string x = Console.ReadLine();
-        if(x.ToLower() == "yes")
-        {
-           Review userReview = Review.CreateReview(customer);
-           
-        }
-        else
-        {
-            Console.WriteLine("Thanks for the visit");
-        }
         
         
-    }
-}    
+    
+    
 
-public class Hotel
-{
-    public void checkout()
+    int menu_select = 1;
+
+    while (true)
     {
-        Console.WriteLine("which booking do you want to checkout?");
-        // print list of customers
-        //give them chance to review
-        //remove them from list and make the room availble
+        System.Console.Clear();
 
+            switch (menu_select)
+            {
+                case 1:
+                    Console.WriteLine("Do you want to book a room" + "<--");
+                    Console.WriteLine("Check in");
+                    Console.WriteLine("Check out");
+                    Console.WriteLine("Show reviews");
+                    Console.WriteLine("quit");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Do you want to book a room");
+                    Console.WriteLine("Check in" + "<--");
+                    Console.WriteLine("Check out");
+                    Console.WriteLine("Show reviews");
+                    Console.WriteLine("quit");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Do you want to book a room");
+                    Console.WriteLine("Check in");
+                    Console.WriteLine("Check out" + "<--");
+                    Console.WriteLine("Show reviews");
+                    Console.WriteLine("quit");
+                    break;
+                case 4:
+                    Console.WriteLine("Do you want to book a room");
+                    Console.WriteLine("Check in");
+                    Console.WriteLine("Check out");
+                    Console.WriteLine("Show reviews" + "<--");
+                    Console.WriteLine("quit");
+                    break;
+                case 5:
+                    Console.WriteLine("Do you want to book a room");
+                    Console.WriteLine("Check in");
+                    Console.WriteLine("Check out");
+                    Console.WriteLine("Show reviews");
+                    Console.WriteLine("quit" + "<--");
+                    break;
+                default:
+                    break;
+            }
+
+            // Code for navigating the menu
+            ConsoleKeyInfo theKey = Console.ReadKey();
+
+        if (theKey.Key == ConsoleKey.DownArrow)
+        {
+            menu_select = Math.Min(menu_select + 1, 5);
+        }
+        else if (theKey.Key == (ConsoleKey.UpArrow))
+        {
+            menu_select = Math.Max(menu_select - 1, 1);
+        }
+        else if (theKey.Key == (ConsoleKey.Enter))
+        {
+            // Methods in the menu
+            switch (menu_select)
+            {
+                case 1:
+                    ();
+                    break;
+
+                case 2:
+                    ();
+                    break;
+
+                case 3:
+                    ();
+                    
+                    break;
+                case 4:
+                    ();
+                    break;
+                case 5:
+                    return;
+            }
+        }
+        }
     }
-}                        
+}
