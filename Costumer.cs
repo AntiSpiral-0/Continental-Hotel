9a6841ce0eb9306170f48686fe5f5a1c8e4cef2e
Yassine Thayer <yassine.ben.thayer.prv@gmail.com>
2023-11-06 14:00:55 +0100

    made class review private

--- Costumer.cs
+++ Costumer.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 
 namespace CustomerManagement
 {
-    public class Customer
+    class Customer
     {
         private int customerId;
         private string name;
--- Review.cs
+++ Review.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace CustomerManagement
 {
-    public class Review
+    class Review
     {
         public Customer Reviewer { get; set; }
         public string Comment { get; set; }
